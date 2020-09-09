58410a236301b4551c3fcea0b3511534f60be859
subhandp <subhan.dinda.putra@gmail.com>
2020-09-09 08:03:05 +0800

    initial

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,15 @@
 import click
+
 from src.string_transformation_01 import lowercase,uppercase,capitalize 
 from src.arithmetic_02 import add,substract,divide,multiply 
+from src.statistic_03 import mean,median,mode,fmean
+from src.palindrome_04 import palindrome
+from src.obfuscator_05 import obfuscate
+from src.random_string_06 import randomStr
+from src.ip_private_07 import ip_private
+from src.ip_public_08 import ip_public
 from src.infinite_inputs_09 import sum_infinite
-
+# from src.crud_10 import create,read,readid,delete
 
 @click.group()
 def cli():
@@ -17,8 +24,27 @@ cli.add_command(substract)
 cli.add_command(divide)
 cli.add_command(multiply)
 
+cli.add_command(mean)
+cli.add_command(median)
+cli.add_command(mode)
+cli.add_command(fmean)
+
+cli.add_command(palindrome)
+
+cli.add_command(obfuscate)
+
+cli.add_command(randomStr)
+
+cli.add_command(ip_private)
+
+cli.add_command(ip_public)
+
 cli.add_command(sum_infinite)
 
+# cli.add_command(create)
+# cli.add_command(read)
+# cli.add_command(readid)
+# cli.add_command(delete)
 
 
 if __name__ == "__main__":
--- src/ip_public_08.py
+++ src/ip_public_08.py
@@ -6,7 +6,7 @@ def cli():
     pass
 @cli.command(name="ip_public")
 def ip_public():
-    ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    ip = urllib.request.urlopen('https://api.ipify.org').read().decode('utf8')
     print(ip)
 
 if __name__ == "__main__":
